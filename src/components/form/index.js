93538cdf3b1d30da973cba934a0e83ad4f90c0b5
antoni <antoni.dev0@gmail.com>
2021-12-21 21:51:32 -0800

    updates app.js to functional component using useSate hook

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -14,18 +14,19 @@ const Form = (props) => {
     if( url === '' ){
       return data = await axios
         .get('https://pokeapi.co/api/v2/pokemon?limit=5')
-        .then(res => res)
+        .then( res => res)
+        .catch(e => console.log('error: ', e.message))
     }else{
       return data = await axios  
         .get(url)
         .then(res => res)
+        .catch( e => console.log('error: ', e.message))
     }
 
   }
   
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     const data = await axiosGet()
     props.handleApiCall({ data: data, method: method, url: url });
 
@@ -37,14 +38,14 @@ const Form = (props) => {
           <label >
             <span> URL </span>
             <input
-              onChange={ () => (e) => setURL(e.target.value) }
+              onChange={ (e) =>  setURL(e.target.value) }
               name='url' 
               type='text' />
             <button 
               type="submit"> GO </button>
           </label>
           <label 
-            onClick={ () => (e) => setMethod(e.target.id) }
+            onClick={ (e) => setMethod(e.target.id) } 
             className="methods">
             <span id="get">GET</span>
             <span id="post">POST</span>
