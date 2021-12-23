cf76f53087c96a45827a99b735d9cacfdae753c4
antoni <antoni.dev0@gmail.com>
2021-12-22 18:52:51 -0800

    adds boot strap dep, creates isLoading state var in form.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,30 @@
+import Card from 'react-bootstrap/Card'
 
-const Header = ({title}) => {
+const Header = ({title, method, url}) => {
 
   return(
-    <header>
-      <h1 
-        data-testid="header-1"
-        className="header">{title}</h1>
-      <h2 
-        title="subtitle" 
-        className="header2"
-      >by: antoni909
-      </h2>
-    </header>
+    <>
+      <header>
+        <h1 
+          data-testid="header-1"
+          className="header">{title}</h1>
+        <h2 
+          title="subtitle" 
+          className="header2"
+        >by: antoni909
+        </h2>
+      </header>
+      <Card style={{ width: "30rem"}}>
+        <Card.Body>
+          <Card.Text>
+            Method: {(method)}
+          </Card.Text>
+          <Card.Text>
+            URL: {(url)}
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
   )
 
 }
