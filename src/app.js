ab31e18acddb4c180f60535cf03b423f84f2437c
antoni <antoni.dev0@gmail.com>
2022-01-05 10:04:10 -0800

    updates app to use useEffect, places fetchAPI function in app and not form component, useEffect only runs on change to requestParams

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './app.scss';
 import Header from './components/header';
 import Footer from './components/footer';
@@ -11,17 +12,65 @@ import Col from 'react-bootstrap/Col'
 const App = () => {
 
   const [data , setData ] = useState(null)
-  const [requestParams , setReqParams ] = useState({})
+  const [requestParams , setRequestParams ] = useState({})
   const [isLoading, setIsLoading] = useState(false)
+
+  const fetchAPI = async (url,method) => {
+    let response = null
+    if( url && (method === 'GET') ){
+      return response = await axios({
+          method: method, 
+          url: url,
+        })  
+          .then(res => res)
+          .catch( e => console.log('error: ', e.message))
+    }else if(url && (method === 'POST')){
+      return response = await axios({
+        method: method, 
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        data: {title:'fake title', body:'fake body', userId: 2}
+      })  
+        .then(res => res)
+        .catch( e => console.log('error: ', e.message))
+    }else if(url && (method === 'PUT')){
+      return response = await axios({
+        method: method, 
+        url: 'https://jsonplaceholder.typicode.com/posts/1',
+        data: {
+          id: 1,
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        }
+      })  
+        .then(res => res)
+        .catch( e => console.log('error: ', e.message))
+    }else if(url && (method === 'DELETE')){
+      return response = await axios({
+        method: method, 
+        url: 'https://jsonplaceholder.typicode.com/posts/1'
+      })  
+        .then( res => res )
+        .catch( e => console.log('error: ', e.message))
+    }else{ return response }
+  }
 
   const callApi = async (reqParams) => {
     setIsLoading(true)
     setTimeout(() => {
-      setData(reqParams.data)
-      setReqParams(reqParams)
+      // setData(reqParams.data)
+      setRequestParams(reqParams)
       setIsLoading(false) 
     }, 1000);
   }
+  
+  useEffect(async () => {
+    if(Object.keys(requestParams).length > 0){
+      const response = await fetchAPI(requestParams.url, requestParams.method)
+      setData(response)
+    }
+  },[requestParams])
+
   return (
     <div>
         <Container fluid>
